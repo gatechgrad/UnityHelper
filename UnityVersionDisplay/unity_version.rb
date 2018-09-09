b88c36d3b0de81e4042dc9fb0786fa378bd7259e
Levi D. Smith <me@levidsmith.com>
2018-09-08 23:39:41 -0400

    Graphical interface update;  Now with checkboxes and selectable games;  Remove text box

--- UnityVersionDisplay/unity_version.rb
+++ UnityVersionDisplay/unity_version.rb
@@ -4,9 +4,28 @@
 PROJECTS_DIR='E:/ldsmith/projects'
 UNITY_CURRENT_VERSION = "2018.2.6f1"
 
+class GameProject
+		attr_accessor :name
+		attr_accessor :versions
+		attr_accessor :hasUnityPackageManagerFolder
+		attr_accessor :hasTempFolder
+		
+		def hasCurrentVersion
+			
+			return true
+		end
+end
+
+class UnityVersion
+	attr_accessor :versionNumber
+	attr_accessor :fileName
+end
+
+
 def displayProjects(strProjectsDir, strCurrentVersion)
+	gameProjectList = Array.new
+	
 	strResult = ""
-#	strResult << "hello"
 	puts "Projects: #{strProjectsDir}"
 	
 	if (!File.directory?(strProjectsDir))
@@ -18,27 +37,33 @@ def displayProjects(strProjectsDir, strCurrentVersion)
 			
 			strEntryPath = File.join(strProjectsDir, entry)
 			if (File.directory?(strEntryPath) && File.directory?(strEntryPath + "/Assets") )
+				game = GameProject.new
 			
 				
-				hasPrintedProjectName = false
+#				hasPrintedProjectName = false
+				game.name = entry
+				game.versions = Array.new
+				
 				Dir.entries(strEntryPath).select { | strFileName |
 					if (strFileName != '.' && strFileName != '..')
 
 						if ((strFileName.end_with? "Editor.csproj") && (strFileName != "Assembly-CSharp-Editor.csproj"))
-#						if (strFileName.end_with? "Editor.csproj")
-							if (!hasPrintedProjectName)
-#								puts "#{entry}"			
-								strResult << "#{entry}" + "\n"
-								hasPrintedProjectName = true
-							end
+#							if (!hasPrintedProjectName)
+#								strResult << "#{entry}" + "\n"
+#								hasPrintedProjectName = true
+#							end
 						
 							strEditorFile = File.join(strEntryPath, strFileName)
 							f = File.open(strEditorFile, "r")
-#							File.open(strEditorFile, "r").each do | line |
 							f.each do | line |
 								if (line =~ /<UnityVersion>(.*)<\/UnityVersion>/)
-#									puts "  Unity Version: " + $1 + " (#{strFileName})"
 									strResult << "  Unity Version: " + $1 + " (#{strFileName})" + "\n"
+
+									version = UnityVersion.new
+									version.versionNumber = $1
+									version.fileName = strFileName
+									game.versions << version
+
 								end
 							
 								
@@ -52,19 +77,21 @@ def displayProjects(strProjectsDir, strCurrentVersion)
 				strProjectVersionFile = 'ProjectSettings/ProjectVersion.txt'
 				strProjectVersionPath = File.join(strEntryPath, strProjectVersionFile)
 				if (File.file?(strProjectVersionPath))
-					if (!hasPrintedProjectName)
-#						puts "#{entry}"			
-						strResult << "#{entry}" + "\n"
-						hasPrintedProjectName = true
-					end
+#					if (!hasPrintedProjectName)
+#						strResult << "#{entry}" + "\n"
+#						hasPrintedProjectName = true
+#					end
 
 					f = File.open(strProjectVersionPath, "r")
-#					File.open(strProjectVersionPath, "r").each do | line |
 					f.each do | line |
 						if (line =~ /m_EditorVersion: (.*)/)
 							strVersion = $1
-#							puts "  Unity Version: " + $1 + " (#{strProjectVersionFile})"
 							strResult << "  Unity Version: " + $1 + " (#{strProjectVersionFile})"
+							
+							version = UnityVersion.new
+							version.versionNumber = $1
+							version.fileName = strProjectVersionFile
+							game.versions << version
 							
 							if (strVersion == strCurrentVersion)
 								strResult << "[ OK ]"
@@ -81,25 +108,25 @@ def displayProjects(strProjectsDir, strCurrentVersion)
 				
 				
 				if (File.directory?(File.join(strEntryPath, "UnityPackageManager")))
-#					puts "  Delete UnityPackageManager directory after upgrading to Unity 2018.2"
 					strResult << "  Delete UnityPackageManager directory after upgrading to Unity 2018.2" + "\n"
 				end
 				
 				if (File.directory?(File.join(strEntryPath, "Temp")))
-#					puts "  Consider deleting Temp folder"
 					strResult << "  Consider deleting Temp folder" + "\n"
 				end
 
+				gameProjectList << game
+				
 			end
 			
 		end
 	}
 	
 	
-#	strResult << "a" + "\n"
-	puts strResult
+#	puts strResult
 	
-	return strResult
+#	return strResult
+	return gameProjectList
 end
 
 
--- UnityVersionDisplay/unity_version_gui.rb
+++ UnityVersionDisplay/unity_version_gui.rb
@@ -4,6 +4,96 @@
 require 'gtk3'
 require_relative 'unity_version'
 
+$tableGames = Gtk::Table.new(1, 1, false)
+$checkboxArray
+$gameArray
+
+
+def tableRemoveAll()
+
+	$tableGames.children.each do | child |
+		$tableGames.remove(child)
+	end
+
+end
+
+
+def makeGameProjectGrid()
+#	gridGames = Gtk::Grid.new
+	
+#	labelTest = Gtk::Label.new
+#	labelTest.label = "Test"
+#	gridGames.attach(labelTest, 0, 0, 1, 1)
+	
+	
+#	return labelTest
+
+end
+
+def makeGameProjectRow(gridGames, iRow, gameProject)
+
+#	textGameProjectName = Gtk::Entry.new
+#	textGameProjectName.text = gameProject.name
+	
+#	textGameProjectVersion = Gtk::Entry.new
+#	textGameProjectVersion.text = "?"
+	
+#	gridGames.attach(textGameProjectName, 0, iRow, 1, 1)
+#	gridGames.attach(textGameProjectVersion, 1, iRow, 1, 1)
+
+	
+
+
+#	gameCheckbox = Gtk::CheckButton.new()
+#	$tableGames.attach(gameCheckbox, 0, 1, iRow, iRow + 1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+#	gameCheckbox.show
+
+#	labelName = Gtk::Label.new(gameProject.name)
+#	$tableGames.attach(labelName, 1, 2, iRow, iRow + 1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+#	labelName.show
+	
+#	labelVersion = Gtk::Label.new("")
+#	gameProject.versions.each do | version |
+#		labelVersion.label = "#{version.versionNumber} (#{version.fileName})"
+#	end
+#	$tableGames.attach(labelVersion, 2, 3, iRow, iRow + 1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+#	labelVersion.show
+
+
+	
+	gameCheckbox = Gtk::CheckButton.new()
+	$checkboxArray << gameCheckbox
+	$tableGames.attach_defaults(gameCheckbox, 0, 1, iRow, iRow + 1)
+	gameCheckbox.show
+
+	labelName = Gtk::Label.new(gameProject.name)
+	$gameArray << gameProject
+	$tableGames.attach_defaults(labelName, 1, 2, iRow, iRow + 1)
+	labelName.show
+	
+	labelVersion = Gtk::Label.new("")
+	gameProject.versions.each do | version |
+#		labelVersion.label = "#{version.versionNumber} (#{version.fileName})"
+		if (labelVersion.label != "")
+			labelVersion.label += ", "
+		end
+		labelVersion.label += "#{version.versionNumber}"
+	end
+	if (labelVersion.label != UNITY_CURRENT_VERSION) 
+		labelVersion.override_background_color(:normal, Gdk::RGBA::new(  1.0, 0.5, 0.5, 1.0))
+	end
+	
+	$tableGames.attach_defaults(labelVersion, 2, 3, iRow, iRow + 1)
+	labelVersion.show
+	
+	$tableGames.set_row_spacing(iRow, 8)
+
+	
+	
+end
+
+
+def makeWindow()
 window = Gtk::Window.new("Unity Version")
 window.set_size_request(640, 480)
 window.set_border_width(10)
@@ -12,6 +102,8 @@ window.set_border_width(10)
 iRow = 0
 
 grid = Gtk::Grid.new
+gridGames = Gtk::Grid.new
+
 
 textResults = Gtk::TextView.new()
 scrolledWindow = Gtk::ScrolledWindow.new()
@@ -34,12 +126,84 @@ textUnityCurrentVersion = Gtk::Entry.new
 textUnityCurrentVersion.text = UNITY_CURRENT_VERSION
 grid.attach(textUnityCurrentVersion, 1, iRow, 1, 1)
 
+
+
+
 iRow += 1
 button = Gtk::Button.new(:label => "Check")
+
+
+$tableGames.attach(Gtk::CheckButton.new(), 0, 1, 0, 1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+$tableGames.attach(Gtk::Label.new("b"), 1, 2, 0, 1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+$tableGames.attach(Gtk::Label.new("c"), 2, 3, 0, 1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+
+$tableGames.attach(Gtk::CheckButton.new(), 0, 1, 1, 2, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+$tableGames.attach(Gtk::Label.new("e"), 1, 2, 1, 2, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+$tableGames.attach(Gtk::Label.new("f"), 2, 3, 1, 2, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+
+
+
+
 button.signal_connect "clicked" do |_widget|
 	puts "Check"
-	textResults.buffer.text = displayProjects(textProjectDirectory.text, textUnityCurrentVersion.text)
+	tableRemoveAll()
+#	textResults.buffer.text = displayProjects(textProjectDirectory.text, textUnityCurrentVersion.text)
+	gameProjectsList = displayProjects(textProjectDirectory.text, textUnityCurrentVersion.text)
+	
+#	$tableGames = Gtk::Table.new(1, 1, false)
+
+	
+	textResults.buffer.text = "Games\n" 
+	iGame = 0
+	
+	$checkboxArray = Array.new
+	$gameArray = Array.new
+
+
+	
+	gameProjectsList.each do | game |
+	
+#		textResults.buffer.text += "Game: #{game.name}\n" 
+		makeGameProjectRow(gridGames, iGame, game)
+		iGame += 1
+	end
+	
+	
+
+	
+#	scrolledWindow.remove(textResults)
+	
+#	labelTest = Gtk::Label.new
+#	labelTest.label = "Test"
+
+#	scrolledWindow.add(labelTest)
+#	scrolledWindow.child_notify
+
+puts "Adding new table row"
+#$tableGames.attach(Gtk::CheckButton.new(), 0, 1, 2, 3, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+#$tableGames.attach(Gtk::Label.new("e"), 1, 2, 2, 3, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+#$tableGames.attach(Gtk::Label.new("f"), 2, 3, 2, 3, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+
+#grid.attach(Gtk::Label.new("f"), 0, 10, 1, 1)
+
+#scrolledWindow.remove($tableGames)
+#$tableGames = Gtk::Table.new(1, 1, false)
+#$tableGames.attach(Gtk::CheckButton.new(), 0, 1, 0, 1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+#$tableGames.attach(Gtk::Label.new("x"), 1, 2, 0, 1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+#$tableGames.attach(Gtk::Label.new("y"), 2, 3, 0, 1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+#scrolledWindow.add($tableGames)
+#mylabel = Gtk::Label.new("y")
+
+#$tableGames.attach(mylabel, 1, 2, 0, 1, Gtk::AttachOptions::EXPAND, Gtk::AttachOptions::SHRINK, 0, 0)
+#mylabel.show
+
+makeGameProjectGrid()
+window.set_title("Update table")
+
+
+	
 end
+
 grid.attach(button, 0, iRow, 1, 1)
 
 
@@ -50,16 +214,52 @@ end
 grid.attach(button, 1, iRow, 1, 1)
 iRow += 1 
 
-textResults.buffer.text = ''
+
+button = Gtk::Button.new(:label => "Display selected")
+button.signal_connect "clicked" do |_widget|
+	i = 0
+	strSelected = ""
+	$checkboxArray.each do | checkbox |
+		if (checkbox.active?)
+			strSelected << "#{$gameArray[i].name}\n"
+		end
+		i += 1
+	end
+	
+	md = Gtk::MessageDialog.new :parent => window,
+		:flags => :destroy_with_parent, :type => :info,
+		:buttons_type => :close, :message => "Games Selected\n" + strSelected
+	md.run
+	md.destroy
+	
+	
+							 
+	
+end
+grid.attach(button, 1, iRow, 1, 1)
+iRow += 1 
+
+
+
+textResults.buffer.text = 'Hello'
 scrolledWindow.min_content_width = 600
 scrolledWindow.min_content_height = 400
 scrolledWindow.vscrollbar_policy = Gtk::PolicyType::ALWAYS
-scrolledWindow.add(textResults)
-#grid.attach(textResults, 0, 2, 1, 1)
+
+#scrolledWindow.add(textResults)
+#scrolledWindow.add(gridGames)
+
+scrolledWindow.add($tableGames)
 grid.attach(scrolledWindow, 0, iRow, 1, 1)
+
+
 
 window.add(grid)
 window.signal_connect("delete-event") { |_widget| Gtk.main_quit }
 window.show_all
 
 Gtk.main
+
+end
+
+makeWindow()
